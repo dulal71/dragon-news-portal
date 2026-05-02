69abbcbeb8198b6328a7f0ace989fe9ed8b2634a
dulal71 <ahmeddulal4211@gmail.com>
2026-05-02 14:28:03 +0600

    add google and github signIn method

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from '@/assets/logo.png'
 import Image from 'next/image';
 const Header = () => {
     return (
-        <div className=" max-w-3xl mx-auto text-center my-10 space-y-2">
+        <div className=" max-w-3xl mx-auto text-center my-10 space-y-2 px-2">
             <Image className="mx-auto"  src={Logo} width={400} height={60}alt='drgon news logo '></Image>
             <p>Journalism Without Fear or Favour</p>
             <p>{format(new Date(), "EEEE,MMMM,yyyy-MM-dd")}</p>
--- src/components/HomePage/Login.jsx
+++ src/components/HomePage/Login.jsx
@@ -1,14 +1,28 @@
+'use client'
+
+import { authClient } from "@/lib/auth-client";
 import { FaGithub } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 const Login = () => {
+  const googleSignUp=async()=>{
+const data = await authClient.signIn.social({
+   provider: "google",
+})
+  }
+  const githubSignUp=async()=>{
+const data = await authClient.signIn.social({
+   provider: "github",
+})
+console.log(data);
+  }
     return (
         <div className="hidden md:block  col-span-3">
      <h2 className="font-semibold text-xl mb-2">
        Login With
      </h2>
     <div className="flex flex-col gap-2 justify-start items-start shadow p-2">
-         <button className="border-2 border-gray-400 px-2 flex  items-center gap-1"><FaGoogle />Login with Google</button>
-       <button className="border-2 border-gray-400 px-2 flex  items-center gap-1"><FaGithub /> Login with Github</button>
+         <button onClick={googleSignUp} className="border-2 border-gray-400 px-2 flex  items-center gap-1"><FaGoogle /> Google</button>
+       <button onClick={githubSignUp} className="border-2 border-gray-400 px-2 flex  items-center gap-1"><FaGithub /> Github</button>
     </div>
     </div> 
     );
